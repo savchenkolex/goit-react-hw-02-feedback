333da567b7d219ed993e45f0954b2fcb0af2e821
lex <savchenko.lex@gmail.com>
2023-08-05 14:24:32 +0300

    fix unused properties in statistics

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,18 +1,19 @@
 import React, {Component} from "react";
-import PropTypes from "prop-types";
+
 import css from "./Statistics.module.css";
 
 
 class Statistics extends Component {
 
+
     render() {
         return (
             <>
             <h2>Statistics</h2>
-            <ul>
-                <li>Good: </li>
-                <li>Neutral: </li>
-                <li>Bad: </li>
+            <ul className={css.votes}>
+                <li className={css.vote}>Good: </li>
+                <li className={css.vote}>Neutral: </li>
+                <li className={css.vote}>Bad: </li>
             </ul>
             </>
         )

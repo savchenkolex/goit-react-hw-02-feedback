36f5cb34ad6a2194bb1e599b82727b3af0f809f4
lex <savchenko.lex@gmail.com>
2023-08-06 02:33:18 +0300

    hide footer

--- src/App.js
+++ src/App.js
@@ -67,7 +67,7 @@ class App extends Component {
             <Notification message="There is no feedback" />}
           </Section>
         </Container>
-        <Footer />
+        {/* <Footer /> */}
       </>
     );
   }
